df2a8967cf6ecd50cd19c4ffc68590dc4c18a8dd
Michal Jansizewski <mjaniszew@gmail.com>
2023-10-13 16:37:30 +0200

    up

--- esp32-temp-sensor/src/main.cpp
+++ esp32-temp-sensor/src/main.cpp
@@ -3,31 +3,31 @@
     Ported to Arduino ESP32 by Evandro Copercini
     updates by chegewara
 */
-
+#include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <BLE2904.h>
+#include <Adafruit_AHTX0.h>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define TEMP_SERVICE_UUID "b0a1671a-7d3e-4fb5-8e6f-79fa1aac6f72"
-#define HUM_SERVICE_UUID "da77106e-88c9-46c5-a73c-ef2deda9a736"
-#define TEMP_CHARACTERISTIC_UUID "3f8a0e0f-fc19-49a9-b653-fbc10db8f806"
-#define HUM_CHARACTERISTIC_UUID "f1f07dee-1d98-4b56-a910-9b5f19f1671a"
+#define SERVICE_UUID (uint16_t)0x181A
+#define TEMP_CHARACTERISTIC_UUID (uint16_t)0x2A6E
+#define HUM_CHARACTERISTIC_UUID (uint16_t)0x2A6F
+#define HUM_DESC_UUID "0000290C-0000-1000-8000-00805F9B34FB"
+
+Adafruit_AHTX0 aht;
 
 BLEServer *sensorServer;
-
-BLEService *tempSensorService;
+BLEService *sensorService;
 BLECharacteristic *tempSensorCharacteristic;
-
-BLEService *humSensorService;
 BLECharacteristic *humSensorCharacteristic;
 
 bool deviceConnected = false;
-float temp = 20;
-float hum = 40;
+sensors_event_t humidity, temp;
 
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -43,63 +43,64 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 
 void readSensorData() {
-  temp++;
-  hum++;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  Serial.print("Temperature: "); 
+  Serial.print(temp.temperature); 
+  Serial.println(" degrees C");
+  Serial.print("Humidity: "); 
+  Serial.print(humidity.relative_humidity); 
+  Serial.println("% rH");
 }
 
 void writeNotify() {
-  tempSensorCharacteristic->setValue(temp);
+  tempSensorCharacteristic->setValue(temp.temperature);
   tempSensorCharacteristic->notify();
-  humSensorCharacteristic->setValue(hum);
+  humSensorCharacteristic->setValue(humidity.relative_humidity);
   humSensorCharacteristic->notify();
 }
 
 void setup() {
   Serial.begin(115200);
+
+  if (! aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    while (1) delay(10);
+  }
+  
+  Serial.println("AHT10 or AHT20 found");
   Serial.println("Starting BLE");
 
   BLEDevice::init("Sensor: T&H");
   sensorServer = BLEDevice::createServer();
   sensorServer->setCallbacks(new MyServerCallbacks());
 
-  tempSensorService = sensorServer->createService(TEMP_SERVICE_UUID);
+  sensorService = sensorServer->createService(SERVICE_UUID);
 
-  tempSensorCharacteristic = tempSensorService->createCharacteristic(
+  tempSensorCharacteristic = sensorService->createCharacteristic(
                                     TEMP_CHARACTERISTIC_UUID,
                                     BLECharacteristic::PROPERTY_READ |
                                     BLECharacteristic::PROPERTY_NOTIFY
                                   );
 
-  tempSensorCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
-  BLE2902* tempSensorDescriptor = new BLE2902();
-  tempSensorDescriptor->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
-  tempSensorCharacteristic->addDescriptor(tempSensorDescriptor);
   tempSensorCharacteristic->setValue("Init temp val");
 
-  humSensorService = sensorServer->createService(HUM_SERVICE_UUID);
-  humSensorCharacteristic = humSensorService->createCharacteristic(
+  humSensorCharacteristic = sensorService->createCharacteristic(
                                     HUM_CHARACTERISTIC_UUID,
                                     BLECharacteristic::PROPERTY_READ |
                                     BLECharacteristic::PROPERTY_NOTIFY
                                   );
 
-  humSensorCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+  humSensorCharacteristic->setValue("Init hum val");
   BLE2902* humSensorDescriptor = new BLE2902();
   humSensorDescriptor->setNotifications(true);
-  humSensorDescriptor->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
   humSensorCharacteristic->addDescriptor(humSensorDescriptor);
-  humSensorCharacteristic->setValue("Init hum val");
 
-  tempSensorService->start();
-  humSensorService->start();
-
-  BLESecurity *sensorSecurity = new BLESecurity();
-  sensorSecurity->setStaticPIN(123456); 
+  sensorService->start();
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(TEMP_SERVICE_UUID);
-  pAdvertising->addServiceUUID(HUM_SERVICE_UUID);
+  pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
 
   BLEDevice::startAdvertising();
   Serial.println("Setup done! Advertising...");
